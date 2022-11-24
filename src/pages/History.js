3bdedc35c9c4622f1394ee113aef78aac7f81b87
shyhhh <hhhtheshy@gmail.com>
2022-11-24 10:26:10 +0800

    上传历史全局状态管理

--- src/pages/History.js
+++ src/pages/History.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import { makeObservable, observable, action, configure } from "mobx";
+import { message } from "antd";
+import { Uploader } from "../models";
 
-const History = () => {
-  return (
-    <div>History</div>
-  )
+configure({
+  enforceActions: "never",
+});
+class HistoryStore {
+  constructor() {
+    makeObservable(this);
+  }
+
+  @observable list = [];
+  @observable isLoading = false;
+  @observable hasMore = true;
+  @observable page = 0;
+  limit = 10;
+
+  @action append(newList) {
+    this.list = this.list.concat(newList);
+  }
+
+  @action find() {
+    this.isLoading = true;
+    Uploader.find({ page: this.page, limit: this.limit })
+      .then((newList) => {
+        this.append(newList);
+        this.page++;
+        if (newList.length < this.limit) {
+          this.hasMore = false;
+        }
+      })
+      .catch((error) => {
+        message.error("加载数据失败");
+      })
+      .finally(() => {
+        this.isLoading = false;
+      });
+    }
+
+  @action reset() {
+    this.list = [];
+    this.isLoading = false;
+    this.hasMore = true;
+    this.page = 0;
+  }
 }
 
-export default History
+export default new HistoryStore();
