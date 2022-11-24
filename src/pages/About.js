581db77411724207022cecdd79b2952f3a3062eb
shyhhh <hhhtheshy@gmail.com>
2022-11-24 20:00:14 +0800

    add about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <div>
-      About
-    </div>
-  )
-}
+      <p> 项目背景:</p>
+      <p> 平时写博客需要用到图床的功能, 于是就写了一个简易版的图床工具</p>
 
-export default About
+      <p> 功能介绍：</p>
+      <p>
+        点击或者拖拽可以上传图片,
+        支持多种格式以及修改图片大小，还可以查看上传历史。
+      </p>
+    </div>
+  );
+};
+
+export default About;
